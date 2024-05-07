3339908f511cb5cd35fed1a37dcf8a18b755eac8
Artyom Pratsko <kabanproff@gmail.com>
2024-05-07 19:28:27 +0300

    before edit about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import styles from "./about.module.css";
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgBox}>
+        {/* <Image src='/about.png' alt={'about'}  width={500} height={500}/> */}
+        {/* <Image src='/about.png' alt={'about'} fill/> */}
+        <Image src='https://images.pexels.com/photos/22590665/pexels-photo-22590665.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt={'about'} fill/>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
