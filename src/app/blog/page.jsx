9970594cf1a93fc59a9bfde3266cb049a9717e8e
Artyom Pratsko <kabanproff@gmail.com>
2024-05-07 19:50:30 +0300

    before single post (detail)

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,17 @@
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
+
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Blog Page</h1>
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+    </div>
+  )
 };
 
 export default BlogPage;
